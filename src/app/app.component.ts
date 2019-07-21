49c0210425eac56becee7a1f2362f1e421ba9992
ImsirovicAjdin <ajdika@live.com>
2019-07-21 05:48:33 +0200

    default pipes, 4 types of data binding in angular, improving our components, an interface is a specification, using an interface as a data type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,60 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 }
+
+/*
+4 types of data binding in Angular:
+####################################################
+Interpolation: {{pageTitle}}
+Property binding: <img [src]='product.imageUrl'>
+Event Binding: <button (click)='toggleImage()'>
+Two-way binding: <input [(ngModel)]='listFilter' />
+
+Checklist: ngModel:
+
+product-list.component.html
+<input type="text" [(ngModel)]="listFilter" />
+
+app.module.ts
+@NgModule({
+  imports: [... FormsModule], <!-- to make the ngModel availabale to all the components in this module
+  declarations: [ AppComponent, ProductListComponent... ] <!-- it's available to these components
+})
+
+
+Default pipes
+#####################################################
+{{ product.price | currency | lowercase }}
+{{ product.price | currency: 'USD':'symbol':'1.2-2' }}
+
+
+Improving our components:
+############################
+
+Strong typing & interfaces
+Encapsulating styles
+Lifecycle hooks
+Custom pipes
+Nested components
+
+Interface is a specification
+#############################
+export interface IProduct {
+  productId: number;
+  productName: string;
+  calculateDiscount(percent: number): number;
+}
+
+Using an interface as a data type
+#################################
+
+import { IProduct } from './product';
+
+export class ProductListComponent {
+  ...
+  // products: any[] = [];
+  products: IProduct[] = [...];
+  ...
+}
+
+*/
